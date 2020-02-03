3d406c565ebc18693a3a286e842c933658868be1
Janus <januslinhc@hotmail.com>
2020-02-03 17:58:39 +0800

    first commit

---
+++ src/Index.ts
@@ -0,0 +1,46 @@
+interface IOptional<T> {
+    value: T | null;
+    isDefined(): boolean;
+    isEmpty(): boolean;
+    nonEmpty(): boolean;
+    getOrElse(value: T): T;
+    get(): T | null;
+}
+
+class Optional<T> implements IOptional<T> {
+    value: T | null = null;
+
+    constructor(value: T | null) {
+        this.value = value;
+    }
+
+    isDefined(): boolean {
+        return this.nonEmpty();
+    }
+    isEmpty(): boolean {
+        return this.value == null;
+    }
+    nonEmpty(): boolean {
+        return !this.isEmpty();
+    }
+    getOrElse(value: T): T {
+        if (this.nonEmpty()) {
+            return this.value!;
+        } else {
+            return value;
+        }
+    }
+    get(): T | null {
+        return this.value;
+    }
+
+    static some<K>(value: K): Optional<K> {
+        return new Optional(value);
+    }
+
+    static none<K>(): Optional<K> {
+        return new Optional<K>(null);
+    }
+}
+
+(window as any).Optional = Optional;
